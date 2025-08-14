e797642556c2251e019a70d543473528beda5d54
Daniel Arnaud <da.arnaud@gmail.com>
2025-08-14 17:36:04 +0200

    Initial Commit

---
+++ KASE-Tester/KASE_TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  KASE_TesterApp.swift
+//  KASE-Tester
+//
+//  Created by Daniel Arnaud on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct KASE_TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
